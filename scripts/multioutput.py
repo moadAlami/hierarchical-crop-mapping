ad648354653b65765456a5edc85b47c1ac82509b
Mouad Alami <moadalami40@gmail.com>
2023-05-17 18:26:27 +0100

    Added multioutput model

--- scripts/multioutput.py
+++ scripts/multioutput.py
@@ -2,15 +2,18 @@ import pandas as pd
 from sklearn.preprocessing import MultiLabelBinarizer, RobustScaler
 from sklearn.multioutput import MultiOutputClassifier
 from sklearn.ensemble import RandomForestClassifier
+import pickle
+import time
 
+start = time.time()
 # tf.random.set_seed(99)
 target_class: str = 'filiere'
 df = pd.read_parquet('../data/ee_sampled_pts_df_2021.parquet')
 
 bands = df.drop(columns=['TRAIN', 'culture', 'filiere']).columns
 df_train, df_test = df.query('TRAIN==True'), df.query('TRAIN==False')
-X_train, y_train = df_train[bands].values, df_train[['culture', 'filiere']].values
-X_test, y_test = df_test[bands].values, df_test[['culture', 'filiere']].values
+X_train, y_train = df_train[bands].values, df_train[['filiere', 'culture']].values
+X_test, y_test = df_test[bands].values, df_test[['filiere', 'culture']].values
 
 scaler = RobustScaler()
 X_train = scaler.fit_transform(X_train)
@@ -24,6 +27,8 @@ forest = RandomForestClassifier(random_state=1)
 multi_target_forest = MultiOutputClassifier(forest, n_jobs=2)
 multi_target_forest.fit(X_train, y_train)
 
-print('y_true:', y_test[0:1])
-y_pred = multi_target_forest.predict(X_test[0:1])
-print('y_pred', y_pred)
+pickle.dump(multi_target_forest, open('../models/multi_target_forest.pickle', 'wb'))
+
+y_pred = multi_target_forest.predict(X_test)
+
+print(f'Execution time: {time.time()-start} seconds.')

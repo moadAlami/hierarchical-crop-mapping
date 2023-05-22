cd95367e272052939d6201075cd4f2272b6f98d4
Mouad Alami <moadalami40@gmail.com>
2023-05-22 17:05:55 +0100

    Load pickled model

--- scripts/test_model_ml.py
+++ scripts/test_model_ml.py
@@ -1,17 +1,17 @@
 import pandas as pd
 from ml_utils import get_xy
-from sklearn.svm import SVC
 from sklearn.metrics import classification_report, confusion_matrix
+import pickle
 
 
 target_class: str = 'filiere'
 df = pd.read_parquet('../data/ee_sampled_pts_df_2021.parquet')
 
 
-X_train, y_train, X_test, y_test, label_encoder = get_xy(df, target_class)
+X_train, X_test, y_train, y_test, label_encoder = get_xy(df, target_class)
 classes = label_encoder.classes_
 
-model = SVC()
+model = pickle.load(open('../models/groups_RandomForestClassifier.pickle', 'rb'))
 model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)

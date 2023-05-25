e836e0ed197e40222eee9f2f2a339d3859400320
Mouad Alami <moadalami40@gmail.com>
2023-05-25 17:19:09 +0100

    Don't use pickled model

--- scripts/test_model_ml.py
+++ scripts/test_model_ml.py
@@ -1,17 +1,20 @@
 import pandas as pd
 from ml_utils import get_xy
+# from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
 from sklearn.metrics import classification_report, confusion_matrix
-import pickle
 
 
-target_class: str = 'filiere'
+target_class: str = 'culture'
 df = pd.read_parquet('../data/ee_sampled_pts_df_2021.parquet')
+df = df.drop(df.query('filiere=="maraicheres"').index)
+df = df.query('filiere=="cereales"')
 
 
 X_train, X_test, y_train, y_test, label_encoder = get_xy(df, target_class)
 classes = label_encoder.classes_
 
-model = pickle.load(open('../models/groups_RandomForestClassifier.pickle', 'rb'))
+model = SVC()
 model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)

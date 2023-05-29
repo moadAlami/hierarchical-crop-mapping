7451c44ee44e289abb3f4c1f3d60a2bdf4e25a38
Mouad Alami <moadalami40@gmail.com>
2023-05-29 14:54:38 +0100

    new updated survey 2021 geometries

--- scripts/ee/spatial_train_test.py
+++ scripts/ee/spatial_train_test.py
@@ -2,10 +2,9 @@ import geopandas as gpd
 import pandas as pd
 import random
 
-gdf_path: str = r'../../data/gharb_2021_plots_wgs.shp'
+gdf_path: str = r'../../data/survey_2021_wgs.shp'
 gdf: gpd.GeoDataFrame = gpd.read_file(gdf_path, encoding='utf8')
 gdf = gdf.rename(mapper={'area': 'AREA'}, axis=1)
-gdf = gdf.drop(columns=['G_TRAIN', 'TRAIN'])
 
 
 def split(gdf):
@@ -23,7 +22,7 @@ def split(gdf):
         if gdf.shape[0] == 3:
             min_poly = 1
         lower_lim = 0.7
-        upper_lim = 0.8
+        upper_lim = 0.9
         if lower_lim < ratio_1 < upper_lim and subset_2.shape[0] >= min_poly:
             subset_1['TRAIN'] = True
             subset_2['TRAIN'] = False
@@ -36,8 +35,10 @@ def split(gdf):
     return new_gdf
 
 
-cultures = ['ble tendre', 'orge', 'ble dur', 'avoine', 'colza', 'grenadier', 'oranger', 'pois chiche', 'feverole', 'melon', 'oignon']
-
+cultures_by_number = gdf.culture.value_counts()[gdf.culture.value_counts() > 1].index
+cultures_by_area = gdf.groupby('culture').AREA.sum()[gdf.groupby('culture').AREA.sum() > 300].index
+cultures = set(cultures_by_number).intersection(set(cultures_by_area))
+print(cultures)
 d: dict = {}
 for culture in cultures:
     d[culture] = split(gdf.query('culture==@culture'))
@@ -45,6 +46,12 @@ for culture in cultures:
 full_df = pd.concat([d[i] for i in d])
 
 full_df = full_df.drop('AREA', axis=1)
-out_gdf = r'../../data/gharb_2021_plots_wgs_v2.shp'
+
+for culture in cultures:
+    train = round(d[culture].query('TRAIN==True').AREA.sum(), 2)
+    test = round(d[culture].query('TRAIN==False').AREA.sum(), 2)
+    print(f'{culture}: {train}/{test}')
+
+out_gdf = r'../../data/gharb_2021_plots_wgs_split.shp'
 
 full_df.to_file(out_gdf, encoding='utf8')

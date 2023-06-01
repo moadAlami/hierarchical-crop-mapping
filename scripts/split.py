71c16a322787170c9d0262c9abc7a39dabd12def
Mouad Alami <moadalami40@gmail.com>
2023-06-01 16:54:08 +0100

    updated split

--- scripts/split.py
+++ scripts/split.py
@@ -3,36 +3,46 @@ import pandas as pd
 
 gdf_path = '../data/GHARB_PARCELS.shp'
 gdf = gpd.read_file(gdf_path, encoding='utf8')
-gdf = gdf.drop('T_Gr', axis=1)
+to_drop = gdf.groupby('culture').COUNT.sum()[gdf.groupby('culture').COUNT.sum() < 300].index
+gdf.loc[gdf.query('culture.isin(@to_drop)').index, 'use'] = 1
+target = 'culture'
+if target == 'culture':
+    thresh = 1
+elif target == 'filiere':
+    thresh = 0
+gdf = gdf.query('use > @thresh').copy()
 
-to_keep = ['Barley', 'Broad beans', 'Common wheat', 'Durum wheat', 'Oats', 'Rapeseed']
-# print(pd.concat([gdf.groupby('CROP').COUNT.sum(), gdf.CROP.value_counts()], axis=1))
-gdf = gdf.query('CROP.isin(@to_keep)')
 
-
-def split(geodataframe):
+def split(geodataframe, target):
     d = {}
-    for crop in geodataframe.CROP.unique():
+    for t in geodataframe[target].unique():
+        d[t] = geodataframe[geodataframe[target] == t]
+        print(t, d[t].COUNT.sum())
         cond = False
         while not cond:
-            d[crop] = geodataframe.query('CROP==@crop')
-            subset_1 = d[crop].sample(frac=.75)
-            subset_2 = d[crop].drop(subset_1.index)
+            subset_1 = d[t].sample(frac=.75)
+            subset_2 = d[t].drop(subset_1.index)
             if subset_1.COUNT.sum() > subset_2.COUNT.sum():
                 subset_1['TRAIN'] = True
                 subset_2['TRAIN'] = False
             else:
                 subset_1['TRAIN'] = False
                 subset_2['TRAIN'] = True
-            d[crop] = pd.concat([subset_1, subset_2])
-            lim_min = 20 * d[crop].COUNT.sum() / 100
-            lim_max = 30 * d[crop].COUNT.sum() / 100
-            test_pixels = d[crop].query('TRAIN==False').COUNT.sum()
+            d[t] = pd.concat([subset_1, subset_2])
+            lim_min = 20 * d[t].COUNT.sum() / 100
+            lim_max = 45 * d[t].COUNT.sum() / 100
+            test_pixels = d[t].query('TRAIN==False').COUNT.sum()
             if lim_min <= test_pixels <= lim_max:
                 cond = True
     full_df = pd.concat([d[i] for i in d])
     return full_df
 
 
-new_gdf = split(gdf).reset_index(drop=True)
-new_gdf.to_file('../data/GHARB_PARCELS_FREQ.shp', encoding='utf8')
+new_gdf = split(gdf, target).reset_index(drop=True)
+
+train_count = new_gdf.query('TRAIN==True').groupby(target).COUNT.sum()
+test_count = new_gdf.query('TRAIN==False').groupby(target).COUNT.sum()
+
+print(pd.concat([train_count, test_count], axis=1))
+
+new_gdf.to_file('../data/GHARB_PARCELS_FREQ_CU.shp', encoding='utf8')

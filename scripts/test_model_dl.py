a361a5fd5330108fc3eb0d701fee17fda12b3a60
Mouad Alami <moadalami40@gmail.com>
2023-06-04 15:45:44 +0100

    Drop 'avoine'

--- scripts/test_model_dl.py
+++ scripts/test_model_dl.py
@@ -1,5 +1,5 @@
 from keras.callbacks import EarlyStopping
-from keras.layers import Input, Conv1D, LSTM, Flatten, Dense, Dropout
+from keras.layers import Input, Conv1D, Flatten, Dense, Dropout
 from keras.models import Model
 from keras.optimizers import Adam
 from keras.utils import to_categorical
@@ -16,21 +16,7 @@ tf.random.set_seed(9)
 target_class = 'culture'
 df = pd.read_parquet('../data/culture_dataset.parquet')
 df = df.query('filiere=="cereales"').copy()
-
-NDVI = df.copy()
-for i in range(14):
-    if i == 0:
-        NDVI.loc[NDVI.index, f'V{i+1}'] = (df['B7'] - df['B3']) / (df['B7'] + df['B3'])
-    else:
-        NDVI.loc[NDVI.index, f'V{i+1}'] = (df[f'B{i}7'] - df[f'B{i}3']) / (df[f'B{i}7'] + df[f'B{i}3'])
-
-ndvi_names = [f'V{i+1}' for i in range(14)]
-NDVI = NDVI[ndvi_names]
-NDVI = NDVI.dropna()
-# bands = [f'B{i+1}' for i in range(140)]
-# df = df.loc[NDVI.index]
-
-# merged_df = pd.concat([NDVI, df], axis=1)
+df = df.drop(df.query('culture=="avoine"').index)
 
 X_train, X_test, y_train, y_test, label_encoder = get_xy(df, target_class)
 classes = label_encoder.classes_
@@ -43,6 +29,8 @@ X_test = X_test.reshape(X_shape)
 
 y_train, y_test = to_categorical(y_train), to_categorical(y_test)
 
+metrics = ['accuracy', 'Recall', 'Precision']
+
 inputs = Input((14, 10))
 hidden = Conv1D(filters=128, kernel_size=1, activation='relu')(inputs)
 hidden = Conv1D(filters=256, kernel_size=1, activation='relu')(hidden)
@@ -51,7 +39,7 @@ hidden = Flatten()(hidden)
 outputs = Dense(units=num_classes, activation='softmax')(hidden)
 model = Model(inputs=inputs, outputs=outputs)
 model.compile(optimizer=Adam(learning_rate=.005, decay=.2),
-              metrics='categorical_accuracy',
+              metrics=metrics,
               loss='categorical_crossentropy')
 
 es = EarlyStopping(monitor='val_loss',
@@ -60,22 +48,24 @@ es = EarlyStopping(monitor='val_loss',
                    restore_best_weights=True,
                    verbose=1)
 
+
 history = model.fit(x=X_train, y=y_train,
                     batch_size=512,
-                    epochs=100,
+                    epochs=1,
                     callbacks=[es],
                     validation_data=(X_test, y_test),
-                    verbose=1)
+                    verbose=0)
 
 y_true = y_test.argmax(axis=1)
-y_pred = model.predict(X_test).argmax(axis=1)
+y_pred = model.predict(X_test, verbose=0).argmax(axis=1)
 report = classification_report(y_true=y_true, y_pred=y_pred, target_names=classes)
 print(report)
 cm = confusion_matrix(y_true=y_true, y_pred=y_pred)
 print(cm)
 
-scores = ['loss', 'categorical_accuracy']
-fig, axs = plt.subplots(1, len(scores))
+metrics = [i.lower() for i in metrics]
+scores = ['loss'] + metrics
+fig, axs = plt.subplots(1, len(scores), figsize=(12, 5))
 c = 0
 for score in scores:
     axs[c].plot(history.history[f'{score}'], label='Training')

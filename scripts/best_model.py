8b9d10a1c65928f0e2842c5314dc70fdc2cc0a69
Mouad Alami <moadalami40@gmail.com>
2023-06-04 15:58:00 +0100

    updated best_model

--- scripts/best_model.py
+++ scripts/best_model.py
@@ -1,14 +1,19 @@
 from ml_utils import pipeline_gridsearch
 import pandas as pd
 
-df_path = '../data/ee_sampled_pts_df_2021.parquet'
-df = pd.read_parquet(df_path)
 
-pipeline_gridsearch(df=df, target_class='filiere')
+df_filiere_path = '../data/filiere_dataset.parquet'
+df_filiere = pd.read_parquet(df_filiere_path)
+df_filiere['culture'] = None
 
-groups = ['legumineuses', 'arboriculture', 'cereales', 'maraicheres']
+df_culture_path = '../data/culture_dataset.parquet'
+df_culture = pd.read_parquet(df_culture_path)
+
+pipeline_gridsearch(df=df_filiere, target_class='filiere')
+
+groups = ['cereales', 'fruitiers', 'legumineuses']
 for group in groups:
-    group_df = df.query(f'filiere=="{group}"')
+    group_df = df_culture.query(f'filiere=="{group}"')
     pipeline_gridsearch(df=group_df, target_class='culture')
 
-pipeline_gridsearch(df=df, target_class='culture')
+pipeline_gridsearch(df=df_culture, target_class='culture')

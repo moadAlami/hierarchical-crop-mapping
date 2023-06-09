2318552f54e4fa1c1ed86b9de9c749a2573fa414
Mouad Alami <moadalami40@gmail.com>
2023-06-09 16:06:37 +0100

    added split function to ml_utils

--- scripts/ml_utils.py
+++ scripts/ml_utils.py
@@ -352,3 +352,28 @@ def custom_tune_v1(x_train, x_test, y_train, y_test, model, grid_params, verbose
     best_model = model(**params[max_f1_index])
     best_model .fit(x_train, y_train)
     return best_model
+
+
+def split(geodataframe, target):
+    d = {}
+    for t in geodataframe[target].unique():
+        d[t] = geodataframe[geodataframe[target] == t]
+        print(t, d[t].COUNT.sum())
+        cond = False
+        while not cond:
+            subset_1 = d[t].sample(frac=.75)
+            subset_2 = d[t].drop(subset_1.index)
+            if subset_1.COUNT.sum() > subset_2.COUNT.sum():
+                subset_1['TRAIN'] = True
+                subset_2['TRAIN'] = False
+            else:
+                subset_1['TRAIN'] = False
+                subset_2['TRAIN'] = True
+            d[t] = pd.concat([subset_1, subset_2])
+            lim_min = 20 * d[t].COUNT.sum() / 100
+            lim_max = 45 * d[t].COUNT.sum() / 100
+            test_pixels = d[t].query('TRAIN==False').COUNT.sum()
+            if lim_min <= test_pixels <= lim_max:
+                cond = True
+    full_df = pd.concat([d[i] for i in d])
+    return full_df
